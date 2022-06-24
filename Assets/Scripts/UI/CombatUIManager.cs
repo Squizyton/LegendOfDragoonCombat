f00cf9418e266b4b76589da4bf9e09ba4b3ce7e4
Joel <fallendemongod@gmail.com>
2022-06-24 13:26:09 -0400

    Other things

--- Assets/Scripts/UI/CombatUIManager.cs
+++ Assets/Scripts/UI/CombatUIManager.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatUIManager : MonoBehaviour
 {
+    public static CombatUIManager instance;
     
+    
+    public Transform characterDetailsContainer;
+
+
+    private void Awake() => instance = this;
 }
