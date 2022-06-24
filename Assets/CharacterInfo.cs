a46eff37ddbf81dadc32431db8fd5ef1eb405787
Joel <fallendemongod@gmail.com>
2022-06-23 22:21:33 -0400

    Inject Info

--- Assets/CharacterInfo.cs
+++ Assets/CharacterInfo.cs
@@ -1,8 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterInfo : MonoBehaviour
 {
+  [Title("Text Objects")] [SerializeField]
+  private TextMeshProUGUI nameText;
+
+  [SerializeField] private TextMeshProUGUI currentHPText;
+  [SerializeField] private TextMeshProUGUI maxHPText;
+  [SerializeField] private TextMeshProUGUI currentMPText;
+  [SerializeField] private TextMeshProUGUI maxMPText;
+
+  [Title("Non text objects")] [SerializeField]
+  private Image characterIcon;
+
+  [SerializeField] private Slider spBar;
   
+  private int currentHP;
+
+
+  public void InjectCharacterInfo(CharacterController character)
+  {
+    nameText.text = character.name;
+    currentHPText.text = character.ReturnHealth().ToString();
+    maxHPText.text = character.ReturnMaxHealth().ToString();
+    currentMPText.text = character.ReturnMana().ToString();
+    maxMPText.text = character.ReturnMaxMana().ToString();
+    characterIcon.sprite = character.character.playerIcon;
+  }
+
+  public void UpdateHP(CharacterController character)
+  {
+    var newHealth = character.ReturnHealth();
+    StartCoroutine(RemoveHealthCoroutine(newHealth));
+  }
+
+  private IEnumerator RemoveHealthCoroutine(int newHealth)
+  {
+    while (currentHP != newHealth)
+    {
+      yield return new WaitForSeconds(.2f);
+      currentHP--;
+      currentHPText.text = currentHP.ToString();
+    }
+  }
 }
--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -7,21 +7,68 @@ public class CharacterController : MonoBehaviour
 {
     public BaseCharacter character;
 
-    public int level;
-    public int health;
-    public int maxHealth;
-    public int mana;
-    public float staminaAmount;
-    public bool readyToAttack;
+    [SerializeField]private int level;
+    [SerializeField]private int health;
+    [SerializeField]private int maxHealth;
+    [SerializeField]private int mana;
+    [SerializeField]private int maxMana;
+    [SerializeField]private float staminaAmount;
+    [SerializeField]private bool readyToAttack;
+    [SerializeField] private CharacterInfo info;
+    
+    
+    private Action onHealthChanged;
+
     public void Start()
     {
         health = character.maxHP;
         maxHealth = character.maxHP;
         mana = character.maxMP;
+
+        onHealthChanged += () => info.UpdateHP(this);
     }
     
     //Stamina tick
+
     
+    /// <summary>
+    /// To be honest, I could just set all variables to public
+    /// </summary>
+    /// <returns></returns>
+    #region Getters
+    public int ReturnHealth()
+    {
+        return health;
+    }
+    public int ReturnMaxHealth()
+    {
+        return maxHealth;
+    }
+    public int ReturnMana()
+    {
+        return mana;
+    }
+    public float ReturnMaxMana()
+    {
+        return maxMana;
+    }
+    public float ReturnStaminaAmount()
+    {
+        return staminaAmount;
+    }
     
+    #endregion
+    
+    public void GetHit(int damage)
+    {
+        health -= damage;
+        
+        if (health <= 0)
+        {
+            health = 0;
+        }
+
+        onHealthChanged?.Invoke();
+    }
     
 }
