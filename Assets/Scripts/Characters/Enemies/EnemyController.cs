9b4fdf0d31f4542da6d06b585d4658616dde162c
Joel <fallendemongod@gmail.com>
2022-06-27 13:18:50 -0400

    EnemyControllers

--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public EnemyInfo enemyInfo;
-    void OnSpawn()
+    
+    
+    [Title("Stats")]
+    [SerializeField]private int health;
+    [SerializeField]private int maxHealth;
+    [SerializeField]private int damage;
+    [SerializeField] private int defense;
+    public void OnSpawn(EnemyInfo info)
     {
-        //Do things in this script
+        enemyInfo = info;
+        health = info.health;
+        maxHealth = info.health;
+        damage = info.damage;
+        defense = info.defense;
+        Instantiate(enemyInfo.prefab, transform);
     }
-
-
+    
+    
     void OnTurnStart()
     {
     }
