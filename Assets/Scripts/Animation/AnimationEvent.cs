62f0d8b88793065fa78948581babbd813d7c9a6b
Joel <fallendemongod@gmail.com>
2022-08-15 17:22:17 -0400

    Update AnimationEvent.cs

--- Assets/Scripts/Animation/AnimationEvent.cs
+++ Assets/Scripts/Animation/AnimationEvent.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class AnimationEvent : MonoBehaviour
 {
+    public bool useForCombat;
+
+    [ShowIf("@useForCombat"),EnableIf("@!hitPlayer")]
+    public bool hitEnemy;
+    [ShowIf("@useForCombat"),EnableIf("@!hitEnemy")]
+    public bool hitPlayer;
+    
+    
+    
+    [ShowIf("@!useForCombat")]
     public UnityEvent TriggerA;
+    [ShowIf("@!useForCombat")]
     public UnityEvent TriggerB;
+    [ShowIf("@!useForCombat")]
     public UnityEvent TriggerC;
+    [ShowIf("@!useForCombat")]
     public UnityEvent TriggerD;
+
+
+    
     
     
     public void TriggerA_Event()
@@ -27,4 +44,12 @@ public class AnimationEvent : MonoBehaviour
     {
         TriggerD?.Invoke();
     }
+
+    public void CombatHit()
+    {
+        if(hitEnemy)
+            CombatManager.instance.HitEnemy();
+        else if (hitPlayer)CombatManager.instance.PlayerHitAnimation();
+    }
+
 }
